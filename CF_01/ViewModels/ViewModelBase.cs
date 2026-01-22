6b4493061120888e8640045458357ee82b1b8ac3
TangLx <TangLx@TANGLX>
2026-01-22 18:08:19 +0700

    Add project files.

---
+++ CF_01/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace CF_01.ViewModels
+{
+    public abstract class ViewModelBase : ObservableObject
+    {
+    }
+}
---
+++ CF_01/Views/Fire.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace CF_01;
+
+public partial class Fire : UserControl
+{
+    public Fire()
+    {
+        InitializeComponent();
+    }
+}
