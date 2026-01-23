6391c5122d3fb626031a853a931ac5ea087f0e15
TangLx <TangLx@TANGLX>
2026-01-23 13:17:46 +0700

    Update Fire

--- CF_01/ViewModels/MainWindowViewModel.cs
+++ CF_01/ViewModels/MainWindowViewModel.cs
@@ -3,5 +3,6 @@
     public partial class MainWindowViewModel : ViewModelBase
     {
         public string Greeting { get; } = "Welcome to Avalonia!";
+
     }
 }
--- CF_01/Views/Fire.axaml.cs
+++ CF_01/Views/Fire.axaml.cs
@@ -1,13 +1,20 @@
-using Avalonia;
+﻿using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using CF_01.ViewModels;
 
-namespace CF_01;
+namespace CF_01.Views;
 
 public partial class Fire : UserControl
 {
     public Fire()
     {
         InitializeComponent();
+        var fire = new FireViewModel();
+        fire.Temperature = 25.0; // Giá trị nhiệt độ ban đầu
+        DataContext = fire;
     }
+
+    // Property để access ViewModel từ bên ngoài
+    public FireViewModel? ViewModel => DataContext as FireViewModel;
 }
